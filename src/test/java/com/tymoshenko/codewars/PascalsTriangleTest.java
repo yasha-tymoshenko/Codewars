4db9a5c6da1072b6f9c705dfe256bced72d622f5
Yakiv Tymoshenko <yakiv.tymoshenko@gmail.com>
2020-12-24 14:17:39 +0100

    PascalsTriangle parameterized tests.

--- src/test/java/com/tymoshenko/codewars/PascalsTriangleTest.java
+++ src/test/java/com/tymoshenko/codewars/PascalsTriangleTest.java
@@ -1,38 +1,36 @@
 package com.tymoshenko.codewars;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 class PascalsTriangleTest {
 
-    @Test
-    void row0() {
-        assertArrayEquals(new int[]{1}, PascalsTriangle.coefficients(0));
+    @ParameterizedTest
+    @MethodSource
+    void pascalsTriangle(int rowNumber, int[] expectedTriangleRow) {
+        assertArrayEquals(expectedTriangleRow, PascalsTriangle.coefficients(rowNumber));
     }
 
-    @Test
-    void row1() {
-        assertArrayEquals(new int[]{1, 1}, PascalsTriangle.coefficients(1));
+    @SuppressWarnings("unused")
+    private static Stream<Arguments> pascalsTriangle() {
+        return Stream.of(
+                pascal(0, 1),
+                pascal(1, 1, 1),
+                pascal(2, 1, 2, 1),
+                pascal(3, 1, 3, 3, 1),
+                pascal(4, 1, 4, 6, 4, 1),
+                pascal(5, 1, 5, 10, 10, 5, 1),
+                pascal(8, 1, 8, 28, 56, 70, 56, 28, 8, 1)
+        );
     }
 
-    @Test
-    void row2() {
-        assertArrayEquals(new int[]{1, 2, 1}, PascalsTriangle.coefficients(2));
-    }
-
-    @Test
-    void row3() {
-        assertArrayEquals(new int[]{1, 3, 3, 1}, PascalsTriangle.coefficients(3));
-    }
-
-    @Test
-    void row4() {
-        assertArrayEquals(new int[]{1, 4, 6, 4, 1}, PascalsTriangle.coefficients(4));
-    }
-
-    @Test
-    void row8() {
-        assertArrayEquals(new int[] {1, 8, 28, 56, 70, 56, 28, 8, 1}, PascalsTriangle.coefficients(8));
+    private static Arguments pascal(int row, int... coefficients) {
+        return of(row, coefficients);
     }
 }
