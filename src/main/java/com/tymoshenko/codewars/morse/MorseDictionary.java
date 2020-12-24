7c47ecc8e6d8d01ece4c2f334a53111857357375
Yakiv Tymoshenko <yakiv.tymoshenko@gmail.com>
2020-12-24 11:49:48 +0100

    4 kyu - RomanNumerals use String repeat.

--- src/main/java/com/tymoshenko/codewars/morse/MorseDictionary.java
+++ src/main/java/com/tymoshenko/codewars/morse/MorseDictionary.java
@@ -5,63 +5,63 @@ import java.util.Map;
 
 public class MorseDictionary {
 
-    private static final Map<String, String> morseToEnglishMap;
+    private static final Map<String, String> dictionary;
 
     public static String translate(String morseCode) {
         return translate(morseCode, "");
     }
 
     public static String translate(String morseCode, String defaultTranslation) {
-        return morseToEnglishMap.getOrDefault(morseCode, defaultTranslation);
+        return dictionary.getOrDefault(morseCode, defaultTranslation);
     }
 
     static {
-        morseToEnglishMap = new LinkedHashMap<>();
+        dictionary = new LinkedHashMap<>();
         // Letters
-        morseToEnglishMap.put(".-", "A");
-        morseToEnglishMap.put("-...", "B");
-        morseToEnglishMap.put("-.-.", "C");
-        morseToEnglishMap.put("-..", "D");
-        morseToEnglishMap.put(".", "E");
-        morseToEnglishMap.put("..-.", "F");
-        morseToEnglishMap.put("--.", "G");
-        morseToEnglishMap.put("....", "H");
-        morseToEnglishMap.put("..", "I");
-        morseToEnglishMap.put(".---", "J");
-        morseToEnglishMap.put("-.-", "K");
-        morseToEnglishMap.put(".-..", "L");
-        morseToEnglishMap.put("--", "M");
-        morseToEnglishMap.put("-.", "N");
-        morseToEnglishMap.put("---", "O");
-        morseToEnglishMap.put(".--.", "P");
-        morseToEnglishMap.put("--.-", "Q");
-        morseToEnglishMap.put(".-.", "R");
-        morseToEnglishMap.put("...", "S");
-        morseToEnglishMap.put("-", "T");
-        morseToEnglishMap.put("..-", "U");
-        morseToEnglishMap.put("...-", "V");
-        morseToEnglishMap.put(".--", "W");
-        morseToEnglishMap.put("-..-", "X");
-        morseToEnglishMap.put("-.--", "Y");
-        morseToEnglishMap.put("--..", "Z");
+        dictionary.put(".-", "A");
+        dictionary.put("-...", "B");
+        dictionary.put("-.-.", "C");
+        dictionary.put("-..", "D");
+        dictionary.put(".", "E");
+        dictionary.put("..-.", "F");
+        dictionary.put("--.", "G");
+        dictionary.put("....", "H");
+        dictionary.put("..", "I");
+        dictionary.put(".---", "J");
+        dictionary.put("-.-", "K");
+        dictionary.put(".-..", "L");
+        dictionary.put("--", "M");
+        dictionary.put("-.", "N");
+        dictionary.put("---", "O");
+        dictionary.put(".--.", "P");
+        dictionary.put("--.-", "Q");
+        dictionary.put(".-.", "R");
+        dictionary.put("...", "S");
+        dictionary.put("-", "T");
+        dictionary.put("..-", "U");
+        dictionary.put("...-", "V");
+        dictionary.put(".--", "W");
+        dictionary.put("-..-", "X");
+        dictionary.put("-.--", "Y");
+        dictionary.put("--..", "Z");
         // Digits
-        morseToEnglishMap.put("-----", "0");
-        morseToEnglishMap.put(".----", "1");
-        morseToEnglishMap.put("..---", "2");
-        morseToEnglishMap.put("...--", "3");
-        morseToEnglishMap.put("....-", "4");
-        morseToEnglishMap.put(".....", "5");
-        morseToEnglishMap.put("-....", "6");
-        morseToEnglishMap.put("--...", "7");
-        morseToEnglishMap.put("---..", "8");
-        morseToEnglishMap.put("----.", "9");
+        dictionary.put("-----", "0");
+        dictionary.put(".----", "1");
+        dictionary.put("..---", "2");
+        dictionary.put("...--", "3");
+        dictionary.put("....-", "4");
+        dictionary.put(".....", "5");
+        dictionary.put("-....", "6");
+        dictionary.put("--...", "7");
+        dictionary.put("---..", "8");
+        dictionary.put("----.", "9");
         // Punctuation
-        morseToEnglishMap.put(".-.-.-", ".");
-        morseToEnglishMap.put("--..--", ",");
-        morseToEnglishMap.put("..--..", "?");
-        morseToEnglishMap.put(".----.", "'");
-        morseToEnglishMap.put("-.-.--", "!");
+        dictionary.put(".-.-.-", ".");
+        dictionary.put("--..--", ",");
+        dictionary.put("..--..", "?");
+        dictionary.put(".----.", "'");
+        dictionary.put("-.-.--", "!");
         // Special codes
-        morseToEnglishMap.put("...---...", "SOS");
+        dictionary.put("...---...", "SOS");
     }
 }
