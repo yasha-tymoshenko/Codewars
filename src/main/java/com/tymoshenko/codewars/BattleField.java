97f58ab8edcdab832fe6ece5b35a48c95426eb79
Yakiv Tymoshenko <yakiv.tymoshenko@gmail.com>
2021-01-22 00:49:09 +0100

    3 kyu - BattleField - refactor.

--- src/main/java/com/tymoshenko/codewars/BattleField.java
+++ src/main/java/com/tymoshenko/codewars/BattleField.java
@@ -4,7 +4,6 @@ package com.tymoshenko.codewars;
 import java.awt.*;
 import java.util.List;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
@@ -18,50 +17,54 @@ import static java.util.stream.Collectors.joining;
  */
 public class BattleField {
 
-    static final int BOARD_SIZE = 10;
+    static final int GAME_MAP_SIZE = 10;
 
     public static boolean fieldValidator(int[][] field) {
         return new BattleField().isValidBattleShipMap(field);
     }
 
-    public boolean isValidBattleShipMap(int[][] field) {
-        if (!isValidSize(field)) {
-            return false;
-        }
+    public boolean isValidBattleShipMap(int[][] gameMap) {
+        boolean valid = false;
         try {
-            Map<ShipType, List<Ship>> shipMap = readShips(field);
-            validateShipTypes(shipMap);
-            return noOverlap(field, shipMap);
+            validateSize(gameMap);
+            Map<ShipType, List<Ship>> ships = readShips(gameMap);
+            validateShipTypes(ships);
+            validateNoShipsOverlap(gameMap, ships);
+            valid = true;
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
-            return false;
+        }
+        return valid;
+    }
+
+    private void validateSize(int[][] gameMap) {
+        if (gameMap.length != GAME_MAP_SIZE
+                || !Arrays.stream(gameMap).allMatch(row -> row.length == GAME_MAP_SIZE)) {
+            throw new IllegalArgumentException(
+                    String.format("Wrong battle field size. Expected %dx%d.", GAME_MAP_SIZE, GAME_MAP_SIZE));
         }
     }
 
-    private boolean isValidSize(int[][] field) {
-        return field.length == BOARD_SIZE && Arrays.stream(field).allMatch(row -> row.length == BOARD_SIZE);
-    }
-
-    private Map<ShipType, List<Ship>> readShips(int[][] field) {
-        List<Ship> shipList = new ArrayList<>();
-        for (int x = 0; x < field.length; x++) {
-            for (int y = 0; y < field.length; y++) {
-                if (field[x][y] == 1) {
-                    Ship ship = createShip(field, new Point(x, y));
-                    shipList.add(ship);
-                    ship.markAsRead(field);
+    private Map<ShipType, List<Ship>> readShips(int[][] gameMap) {
+        List<Ship> ships = new LinkedList<>();
+        for (int x = 0; x < gameMap.length; x++) {
+            for (int y = 0; y < gameMap.length; y++) {
+                if (gameMap[x][y] == 1) {
+                    Ship ship = Ship.create(gameMap, x, y);
+                    ships.add(ship);
+                    ship.markAsRead(gameMap);
                 }
             }
         }
-        return shipList.stream().collect(groupingBy(ShipType::forShip));
+        return ships.stream().collect(groupingBy(ShipType::forShip));
     }
 
-    private void validateShipTypes(Map<ShipType, List<Ship>> shipMap) {
+    private void validateShipTypes(Map<ShipType, List<Ship>> ships) {
         for (ShipType shipType : ShipType.values()) {
-            if (!shipMap.containsKey(shipType)) {
+            if (!ships.containsKey(shipType)) {
                 throw new IllegalArgumentException("Missing ships of type " + shipType);
             }
-            int actualShipsNumber = shipMap.get(shipType).size();
+            int actualShipsNumber = ships.get(shipType).size();
             if (actualShipsNumber != shipType.getShipsNumber()) {
                 throw new IllegalArgumentException(
                         String.format("Wrong amount of ships of type %s. Expected %d but got %d.",
@@ -70,39 +73,49 @@ public class BattleField {
         }
     }
 
-    private boolean noOverlap(int[][] field, Map<ShipType, List<Ship>> shipMap) {
-        return shipMap.values().stream()
+    private void validateNoShipsOverlap(int[][] field, Map<ShipType, List<Ship>> shipMap) {
+        if (shipMap.values().stream()
                 .flatMap(Collection::stream)
                 .flatMap(ship -> ship.borders().stream())
-                .allMatch(borderCell -> field[borderCell.x][borderCell.y] == 0);
-    }
-
-    private Ship createShip(int[][] field, Point cell) {
-        Ship ship = new Ship(cell);
-        int dy = cell.y + 1;
-        while (dy < field.length && field[cell.x][dy] == 1) {
-            ship.addCell(new Point(cell.x, dy));
-            dy++;
+                .anyMatch(borderCell -> field[borderCell.x][borderCell.y] != 0)) {
+            throw new IllegalArgumentException("Some ships overlap.");
         }
-        int dx = cell.x + 1;
-        while (dx < field.length && field[dx][cell.y] == 1) {
-            ship.addCell(new Point(dx, cell.y));
-            dx++;
-        }
-        return ship;
     }
 }
 
 class Ship {
     private final List<Point> cells;
 
-    Ship(Point cell) {
+    private Ship(Point cell) {
         cells = new ArrayList<>();
         cells.add(cell);
     }
 
-    void addCell(Point cell) {
-        cells.add(cell);
+    static Ship create(int[][] gameMap, int x, int y) {
+        Point firstCell = new Point(x, y);
+        Ship ship = new Ship(firstCell);
+        int dy = firstCell.y + 1;
+        while (dy < gameMap.length && gameMap[firstCell.x][dy] == 1) {
+            ship.addCell(firstCell.x, dy);
+            dy++;
+        }
+        int dx = firstCell.x + 1;
+        while (dx < gameMap.length && gameMap[dx][firstCell.y] == 1) {
+            ship.addCell(dx, firstCell.y);
+            dx++;
+        }
+        return ship;
+    }
+
+    @Override
+    public String toString() {
+        return cells.stream()
+                .map(cell -> String.format("(%d, %d)", cell.x, cell.y))
+                .collect(joining(", "));
+    }
+
+    void addCell(int x, int y) {
+        cells.add(new Point(x, y));
     }
 
     void markAsRead(int[][] field) {
@@ -111,47 +124,35 @@ class Ship {
 
     List<Point> borders() {
         List<Point> borders = new ArrayList<>();
-        Point first = cells.get(0);
-        Point last = cells.get(cells.size() - 1);
+        Point firstCell = cells.get(0);
+        Point lastCell = cells.get(cells.size() - 1);
         int iMin;
         int iMax;
         int jMin;
         int jMax;
-        boolean horizontal = first.x == last.x;
-        if (horizontal) {
-            iMin = first.y - 1;
-            iMax = last.y + 1;
-            jMin = first.x - 1;
-            jMax = first.x + 1;
-        } else {
-            iMin = first.x - 1;
-            iMax = last.x + 1;
-            jMin = first.y - 1;
-            jMax = first.y + 1;
-        }
+        boolean horizontal = firstCell.x == lastCell.x;
+        iMin = horizontal ? firstCell.y - 1 : firstCell.x - 1;
+        iMax = horizontal ? lastCell.y + 1 : lastCell.x + 1;
+        jMin = horizontal ? firstCell.x - 1 : firstCell.y - 1;
+        jMax = horizontal ? firstCell.x + 1 : firstCell.y + 1;
         for (int i = iMin; i <= iMax; i++) {
             for (int j = jMin; j <= jMax; j++) {
                 Point cell = horizontal ? new Point(j, i) : new Point(i, j);
-                if (!cells.contains(cell)) {
+                if (isWithinMap(cell) && !cells.contains(cell)) {
                     borders.add(cell);
                 }
             }
         }
-        return borders.stream()
-                .filter(cell -> cell.x >= 0 && cell.x < BattleField.BOARD_SIZE
-                        && cell.y >= 0 && cell.y < BattleField.BOARD_SIZE)
-                .collect(Collectors.toList());
+        return borders;
     }
 
     int size() {
         return cells.size();
     }
 
-    @Override
-    public String toString() {
-        return cells.stream()
-                .map(cell -> String.format("(%d, %d)", cell.x, cell.y))
-                .collect(joining(", "));
+    private boolean isWithinMap(Point cell) {
+        return cell.x >= 0 && cell.x < BattleField.GAME_MAP_SIZE
+                && cell.y >= 0 && cell.y < BattleField.GAME_MAP_SIZE;
     }
 }
 
